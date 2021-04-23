5cb4adb25f20a9db44247c8db550075b5c6cc92a
KAZUKISHUTO <kat0yu0704@gmail.com>
2021-04-23 23:12:02 +0900

    更新

--- scripts/JS/parts2.js
+++ scripts/JS/parts2.js
@@ -5,7 +5,7 @@ $('.slider').slick({
   autoplay: true, //自動的に動き出すか。初期値はfalse。
   infinite: true, //スライドをループさせるかどうか。初期値はtrue。
   slidesToShow: 4, //スライドを画面に3枚見せる
-  slidesToScroll: 1, //1回のスクロールで3枚の写真を移動して見せる
+  slidesToScroll: 4, //1回のスクロールで3枚の写真を移動して見せる
   prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
   nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
   dots: true, //下部ドットナビゲーションの表示
