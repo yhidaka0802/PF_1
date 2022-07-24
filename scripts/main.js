cb1e243aa0d6bf9f991f8809cb650b44647ca16b
yu.hidaka <light3223b@gmail.com>
2022-07-24 10:28:07 +0900

    提出用ver1

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,20 @@ $.scrollify ({
   easing: "swing",
   scrollSpeed: 300,
 })
+
+function fadeIn(){
+  $('.fadeInTrigger').each(function(){
+    let elemPos = $(this).offset().top-50;
+    let scroll = $(window).scrollTop();
+    let windowHeight = $(window).height();
+    if(scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeIn');
+    }else{
+      $(this).removeClass('fadeIn');
+    }
+  });
+}
+
+$(window).scroll(function (){
+  fadeIn();
+});
